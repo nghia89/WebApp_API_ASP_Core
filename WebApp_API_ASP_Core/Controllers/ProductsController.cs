e38df81c1ed1b2f01f6a19c891c8f5b0a8363b2a
Nghia Nguyen <37038823+nghia89@users.noreply.github.com>
2019-07-18 14:57:51 +0700

    add getAll and post

--- WebApp_API_ASP_Core/Controllers/ProductsController.cs
+++ WebApp_API_ASP_Core/Controllers/ProductsController.cs
@@ -1,49 +1,65 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using WebApp.Business.interfaces;
 using WebApp.Business.Model;
+using WebApp.Data.EF.Entities;
 using WebApp_API_ASP_Core.Extensions;
+using WebApp_API_ASP_Core.Model;
 
 namespace WebApp_API_ASP_Core.Controllers
 {
-    [Route("api/[controller]")]
-	[ApiController]
-	[MiddlewareFilter(typeof(LocalizationPipeline))]
-	public class ProductsController : ControllerBase
+    public class ProductsController : Controller
     {
-		private readonly IProductBusiness _productBusiness;
-		public ProductsController(IProductBusiness productBusiness)
-		{
-			this._productBusiness = productBusiness;
-		}
+        private readonly IProductBusiness _productBusiness;
+        public ProductsController(IProductBusiness productBusiness)
+        {
+            this._productBusiness = productBusiness;
+        }
         // GET: api/Products
         [HttpGet]
-        public async  Task<IActionResult> Get()
+        [Route("product/getAll")]
+        [ProducesResponseType(typeof(ProductViewModel), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get()
         {
-			var data=await _productBusiness.GetAll();
-			return new OkObjectResult(data);
+            var data = await _productBusiness.GetAll();
+            var dataResult = data.Select(x => ProductViewModel.from(x)).ToList();
+            return  new JsonResult(dataResult);
 
-		}
+        }
         // POST: api/Products
+
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("product")]
+        [ProducesResponseType(typeof(ProductViewModel), (int)HttpStatusCode.Created)]
+        public async Task<IActionResult> add([FromBody]ProductViewModel model)
         {
-        }
+            var product = new ProductModel()
+            {
+                Name = model.Name,
+                Content = model.Content,
+                DateCreated = DateTime.UtcNow.AddHours(7).Date,
+                Description = model.Description,
+                HomeFlag = model.HomeFlag,
+                HotFlag = model.HotFlag,
+                Image = model.Image,
+                OriginalPrice = model.OriginalPrice,
+                Price = model.Price,
+                ProductCategoryId = model.ProductCategoryId,
+                Tags = model.Tags,
+                SeoAlias = model.SeoAlias,
+                SeoDescription = model.SeoDescription,
+                SeoKeywords = model.SeoKeywords,
+                SeoPageTitle = model.SeoPageTitle,
+                Status = model.Status
 
-        // PUT: api/Products/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+            };
 
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            ProductModel data = await _productBusiness.Add(product);
+            return new OkObjectResult(data);
         }
     }
 }
---
+++ WebApp_API_ASP_Core/Model/ProductViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Business.Model;
+using WebApp.Data.EF.Entities;
+using WebApp.Data.EF.Enums;
+
+namespace WebApp_API_ASP_Core.Model
+{
+	public class ProductViewModel
+	{
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public double? PromotionPrice { get; set; }
+        public double? OriginalPrice { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+        public int? ViewCount { get; set; }
+        public string Tags { get; set; }
+        public string Unit { get; set; }
+        public ProductCategory ProductCategory { set; get; }
+        public long? ProductCategoryId { get; set; }
+        public List<ProductTag> ProductTags { set; get; }
+        public long? ProductTagId { get; set; }
+        public string SeoPageTitle { set; get; }
+        public string SeoAlias { set; get; }
+        public string SeoKeywords { set; get; }
+        public string SeoDescription { set; get; }
+        public Status Status { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+
+        public static ProductViewModel from(ProductModel model)
+        {
+            if (model == null)
+                return null;
+            return new ProductViewModel()
+            {
+                Id=model.Id,
+                Name=model.Name,
+                Price=model.Price,
+                ProductCategoryId=model.ProductCategory.Id,
+                PromotionPrice=model.PromotionPrice,
+                SeoAlias=model.SeoAlias,
+                SeoKeywords=model.SeoKeywords,
+                Status=model.Status,
+                ViewCount=model.ViewCount
+
+            };
+        }
+    }
+}
